aefac108031d65a7b1fb3161571be7018cf6b02d
NateNolting <me@natenolting.com>
2018-01-23 22:42:52 -0600

    Added travis ci build script

--- syllabus.sh
+++ syllabus.sh
@@ -6,6 +6,6 @@ php -r "require_once __DIR__ . '/vendor/autoload.php'; \$schedule= new \\Helpers
 && php -r "require_once __DIR__ . '/vendor/autoload.php'; \$assignments= new \\Helpers\\Generate\\GenerateCourseAssignment(['2018-01-09','2018-04-24','2,4', exec('pwd')]); \$assignments->generate();" \
 && php -r "require_once __DIR__ . '/vendor/autoload.php';  \$syllabus= new \\Helpers\\Generate\\GenerateSyllabus(['directory' => exec('pwd')]); \$syllabus->generate();"
 # generate the TOC
-node_modules/.bin/markdown-toc -i --maxdepth 3 syllabus.md
+yarn run markdown-toc -i --maxdepth 3 syllabus.md
 # copy over the content from course_syllabus.md to README.md
 cat syllabus.md > README.md
