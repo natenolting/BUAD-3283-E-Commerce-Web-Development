30e3795b4edac8643011804321d0d162d633df24
Nate Nolting <me@natenolting.com>
2018-01-25 12:50:54 -0600

    removed unneeded files from repo

--- gulpfile.js
+++ gulpfile.js
@@ -53,14 +53,13 @@ function renderSyllabusToHtml() {
             '<meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">' +
             '<title>' + packageData.name.replace('-', ' ') + '</title>' +
             '<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">' +
-            '<style media="screen">'+ fs.readFileSync('./style/style.css') +'</style>' +
           '</head>' +
           '<body>' +
             '<div class="container">')
         )
           .pipe(insert.append('</div></body>'+
           '</html>'))
-          .pipe(replace('<table', '<table class="table table-bordered-bottom"'))
+          .pipe(replace('<table', '<table class="table table-bordered"'))
           .pipe(gulp.dest('./'));
       resolve();
     });
