d00ccb9f1049a46577e53177123550c1bf4b798f
Nate Nolting <me@natenolting.com>
2018-02-27 13:54:32 -0600

    removed dd

--- public_html/gateway/index.php
+++ public_html/gateway/index.php
@@ -1,7 +1,6 @@
 <?php
 require_once dirname(dirname(__DIR__)) . '/src/bootstrap.php';
 $title = substr(__FILE__, strlen(__DIR__ . DIRECTORY_SEPARATOR), strlen(__FILE__));
-dd($title);
 $body = '
 <h2>Submit a payment</h2>
 <form action="endpoint.php" method="post">
